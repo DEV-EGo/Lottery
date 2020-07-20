297938fe2d49df32d08195bd218f1d8de188f102
edwin gonzalez <edwin.gonzalez11@hotmail.com>
2020-07-20 14:02:13 -0400

    update

--- manifest.js
+++ manifest.js
@@ -1,10 +1,15 @@
 $(document).ready(function () {
+
     $("#random-button").on("click", function () {
+
         var Lottery = "";
 
         for (var i = 0; i < 9; i++) {
+
             var randomGenerate = Math.floor(Math.random() * 9) + 1;
+
             Lottery = randomGenerate + Lottery;
+
         }
 
         $("#random-number").prepend("<br><hr>" + Lottery);
