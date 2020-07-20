9e410e2534d4d6bffb1bc8f22923daf564f011a4
edwin gonzalez <edwin.gonzalez11@hotmail.com>
2020-07-20 14:21:59 -0400

    working product deployed

--- manifest.js
+++ manifest.js
@@ -13,5 +13,8 @@ $(document).ready(function () {
         }
 
         $("#random-number").prepend("<br><hr>" + Lottery);
+        $("#image-div").html("<img src= 'https://i0.wp.com/newportbuzz.s3.amazonaws.com/uploads/2016/01/Powerball-Winning-Numbers.png?fit=650%2C400&ssl=1' />");
+
+        alert("CONGRATULATIONS");
     });
 });
